49d59934157031cfe6516cb7a3fe9002124b001e
Suzal3579 <pe.messh@gmail.com>
2017-01-26 11:05:08 +0530

    Start up alert dialog pop up ...

--- app/src/main/java/com/example/a13000/testappsensor/MainActivity.java
+++ app/src/main/java/com/example/a13000/testappsensor/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a13000.testappsensor;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkFirstRun();
 
         View backgroundImage = findViewById(R.id.activity_main);
         signInNotAccount = (TextView) findViewById(R.id.signUpNoAcc);
@@ -41,5 +44,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         gradientBackgroundPainter.stop();
+    }
+
+    public void checkFirstRun() {
+        boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("isFirstRun", true);
+        if (isFirstRun) {
+            String title = "Welcome to Complaint Board.\n";
+            String message = "App under development for final year project.\n\n\n-By Milan Pander";
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setCancelable(true);
+            builder.setTitle(title);
+            builder.setMessage(message);
+            builder.show();
+            getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                    .edit()
+                    .putBoolean("isFirstRun", false)
+                    .apply();
+        }
     }
 }
