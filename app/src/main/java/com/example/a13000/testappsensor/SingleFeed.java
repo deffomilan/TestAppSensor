e3e6252822510a59b243fddc6787346d6c660f9a
Suzal3579 <pe.messh@gmail.com>
2017-02-12 11:04:42 +0530

    Remove post by that single user ... only that user ...

--- app/src/main/java/com/example/a13000/testappsensor/SingleFeed.java
+++ app/src/main/java/com/example/a13000/testappsensor/SingleFeed.java
@@ -2,9 +2,12 @@ package com.example.a13000.testappsensor;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,8 +21,10 @@ public class SingleFeed extends AppCompatActivity {
 
     private ImageView imageView;
     private TextView title,desc,by;
+    private Button remove_button;
 
     private DatabaseReference databaseRef;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +35,11 @@ public class SingleFeed extends AppCompatActivity {
         title = (TextView) findViewById(R.id.title_place);
         desc = (TextView) findViewById(R.id.description_place);
         by = (TextView) findViewById(R.id.username);
+        remove_button = (Button) findViewById(R.id.remove_post);
 
         key_id = getIntent().getStringExtra("key_id");
         databaseRef = FirebaseDatabase.getInstance().getReference().child("ComplaintHead");
+        firebaseAuth = FirebaseAuth.getInstance();
         databaseRef.keepSynced(true);
 
         databaseRef.child(key_id).addValueEventListener(new ValueEventListener() {
@@ -53,6 +60,18 @@ public class SingleFeed extends AppCompatActivity {
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        // Making button visible for user that is logged in ...
+        if(firebaseAuth.getCurrentUser().getUid().equals(key_id)){
+            remove_button.setVisibility(View.VISIBLE);
+        }
+
+        remove_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
             }
         });
